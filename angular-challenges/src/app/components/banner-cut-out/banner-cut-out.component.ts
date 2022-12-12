f38b41702d06a1ea7bbe12e8de66f7fa8b6eca7d
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-12-12 01:12:29 +0100

    Angular Challenges

--- angular-challenges/src/app/components/banner-cut-out/banner-cut-out.component.ts
+++ angular-challenges/src/app/components/banner-cut-out/banner-cut-out.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner-cut-out',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./banner-cut-out.component.scss']
 })
 export class BannerCutOutComponent {
-
+  @Input() public imageUrl = 'https://images.unsplash.com/photo-1555952494-efd681c7e3f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80';
+  @Input() public title = '100 Angular Challenge';
 }
